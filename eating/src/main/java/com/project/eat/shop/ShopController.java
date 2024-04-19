e67592c05c23764e2237204efb99c5c9b2493254
BEANyyy <yevin2001@gmail.com>
2024-04-19 18:11:45 +0900

    오류 수정

--- eating/src/main/java/com/project/eat/shop/ShopController.java
+++ eating/src/main/java/com/project/eat/shop/ShopController.java
@@ -3,8 +3,6 @@ package com.project.eat.shop;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.tomcat.util.json.JSONParser;
 import org.apache.tomcat.util.json.ParseException;
-import org.json.JSONArray;
-import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,7 +17,6 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
-import org.json.JSONObject;
 
 @Controller
 @Slf4j
