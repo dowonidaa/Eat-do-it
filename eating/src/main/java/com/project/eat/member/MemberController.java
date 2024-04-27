9b06cfb0bfc2d3bff4cb7bc650feb43fb77f4301
dowonidaa <tmddjs2980@naver.com>
2024-04-27 14:41:49 +0900

    - 회원가입 아이디 중복체크 적용

--- eating/src/main/java/com/project/eat/member/MemberController.java
+++ eating/src/main/java/com/project/eat/member/MemberController.java
@@ -11,10 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.lang.reflect.Member;
@@ -135,7 +132,10 @@ public class MemberController {
 
 
     @GetMapping("/member/checkId")
+    @ResponseBody
     public ResponseEntity<Boolean> checkId(@RequestParam String id) {
+        log.info("id check");
+
         boolean isAvailable = service.doesMemberExist(id);
         return ResponseEntity.ok(isAvailable);
     }
